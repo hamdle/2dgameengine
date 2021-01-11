f41221e151c2664e7e620b16473de08190b2854e
hamdle <contact@ericmarty.dev>
2021-01-10 17:41:54 -0800

    Test dependencies with gcc.

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_ttf.h>
+#include <SDL2/SDL_mixer.h>
+#include <glm/glm.hpp>
+#include <imgui/imgui.h>
+#include <sol/sol.hpp>
 
 int main() {
+    sol::state lua;
+    lua.open_libraries(sol::lib::base);
     SDL_Init(SDL_INIT_EVERYTHING);
+
     std::cout << "Hello, world!" << std::endl;
-    std::cout << "Meow" << std::endl;
     return 0;
 }
