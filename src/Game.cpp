fb5b2e315cfadd716ec9ccfe02d9d7cb62edb308
hamdle <contact@ericmarty.dev>
2021-01-10 19:11:51 -0800

    Render SDL window.

--- src/Game.cpp
+++ src/Game.cpp
@@ -1,20 +1,41 @@
 #include "Game.h"
+#include <SDL2/SDL.h>
 #include <iostream>
 
 Game::Game() {
-    std::cout << "Game const called." << std::endl;
+    isRunning = false;
 }
 
 Game::~Game() {
-    std::cout << "Game destruct called." << std::endl;
 }
 
 void Game::Initialize() {
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+        std::cerr << "Error initializing SDL." << std::endl;
+        return;
+    }
+    window = SDL_CreateWindow(
+        "2D Game Engine",
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED,
+        600,
+        400,
+        SDL_WINDOW_BORDERLESS
+    );
+    if (!window) {
+        std::cerr << "Error creating SDL window." << std::endl;
+        return;
+    }
+    renderer = SDL_CreateRenderer(window, -1, 0);
+    if (!renderer) {
+        std::cerr << "Error creating SDL renderer." << std::endl;
+    }
 
+    isRunning = true;
 }
 
 void Game::Run() {
-    while (true) {
+    while (isRunning) {
         ProcessInput();
         Update();
         Render();
@@ -22,17 +43,36 @@ void Game::Run() {
 }
 
 void Game::ProcessInput() {
-
+    SDL_Event sdlEvent;
+    while (SDL_PollEvent(&sdlEvent)) {
+        switch (sdlEvent.type) {
+            case SDL_QUIT:
+                isRunning = false;
+                break;
+            case SDL_KEYDOWN:
+                if (sdlEvent.key.keysym.sym == SDLK_ESCAPE) {
+                    isRunning = false;
+                }
+                break;
+        }
+    }
 }
 
 void Game::Update() {
-
+    // TODO: Update game objects.
 }
 
 void Game::Render() {
+    SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
+    SDL_RenderClear(renderer);
 
+    // TODO: Render game objects here.
+
+    SDL_RenderPresent(renderer);
 }
 
 void Game::Destroy() {
-
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
 }
