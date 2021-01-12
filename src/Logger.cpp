c9b0da57c91d09d7d3abe46e0834554a6d8d28ca
hamdle <contact@ericmarty.dev>
2021-01-11 21:34:56 -0800

    Implement logging functions.

--- src/Logger.cpp
+++ src/Logger.cpp
@@ -2,11 +2,30 @@
 
 #include <iostream>
 #include <string>
+#include <chrono>
+#include <ctime>
+
+std::vector<LogEntry> Logger::messages;
+
+std::string CurrentDateTimeToString() {
+    std::time_t now = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+    std::string output(30, '\0'); 
+    std::strftime(&output[0], output.size(), "%d-%b-%Y %H:%M:%S", std::localtime(&now));
+    return output;
+}
 
 void Logger::Log(const std::string& message) {
-    std::cout << message << std::endl;
+    LogEntry logEntry;
+    logEntry.type = LOG_INFO;
+    logEntry.message = "LOG [" + CurrentDateTimeToString() + "]: " + message;
+    std::cout << "\x1B[32m" << logEntry.message << "\033[0m" << std::endl;
+    messages.push_back(logEntry);
 }
 
 void Logger::Error(const std::string& message) {
-    std::cout << message << std::endl;
+    LogEntry logEntry;
+    logEntry.type = LOG_ERROR;
+    logEntry.message = "ERROR [" + CurrentDateTimeToString() + "]: " + message;
+    messages.push_back(logEntry);
+    std::cerr << "\x1B[91m" << logEntry.message << "\033[0m" << std::endl;
 }
